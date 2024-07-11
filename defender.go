f9a1b05c842a639cbd5083601c1274bcd07396fa
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-07-11 13:51:29 +0200

    [defender] defender struct and methods

--- defender.go
+++ defender.go
@@ -1,1 +1,121 @@
 package go_defender
+
+import (
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+const Factor = 10
+
+type Defender[T comparable] struct {
+	clients map[T]*Client[T]
+
+	Duration    time.Duration
+	BanDuration time.Duration
+	Max         int
+
+	sync.Mutex
+}
+
+func NewDefender[T comparable](max int, duration, banDuration time.Duration) *Defender[T] {
+	return &Defender[T]{
+		clients:     map[T]*Client[T]{},
+		Duration:    duration,
+		BanDuration: banDuration,
+		Max:         max,
+		Mutex:       sync.Mutex{},
+	}
+}
+
+func (d *Defender[T]) BanList() []*Client[T] {
+	banList := make([]*Client[T], 0, len(d.clients))
+
+	for _, client := range d.clients {
+		if client.banned {
+			banList = append(banList, client)
+		}
+	}
+
+	return banList
+}
+
+func (d *Defender[T]) Client(key T) (*Client[T], bool) {
+	d.Lock()
+	defer d.Unlock()
+	client, ok := d.clients[key]
+	return client, ok
+}
+
+func (d *Defender[T]) newLimiter() *rate.Limiter {
+	return rate.NewLimiter(
+		rate.Every(d.Duration),
+		d.Max,
+	)
+}
+
+func (d *Defender[T]) newClientExpiration(now time.Time, duration time.Duration) time.Time {
+	return now.Add(duration)
+}
+
+func (d *Defender[T]) newValidClientExpiration(now time.Time) time.Time {
+	return d.newClientExpiration(now, d.Duration*Factor)
+}
+
+func (d *Defender[T]) newBannedClientExpiration(now time.Time) time.Time {
+	return d.newClientExpiration(now, d.BanDuration)
+}
+
+func (d *Defender[T]) Increment(key T, now time.Time) bool {
+	d.Lock()
+	defer d.Unlock()
+
+	client, found := d.clients[key]
+
+	if !found {
+		d.clients[key] = NewClient(
+			key,
+			d.newLimiter(),
+			d.newValidClientExpiration(now),
+		)
+		return false
+	}
+
+	// Check if the client is not banned anymore and the cleanup hasn't been run yet
+	if client.BanExpired() {
+		client.Unban()
+	}
+
+	// Check if client is banned
+	if client.Banned() {
+		return true
+	}
+
+	// Update the client expiration
+	client.SetExpiration(
+		d.newValidClientExpiration(now),
+	)
+
+	// Check the rate limiter
+	if client.ReachedLimit() {
+		client.Ban()
+		client.SetExpiration(
+			d.newBannedClientExpiration(now),
+		)
+		return true
+	}
+
+	return false
+}
+
+func (d *Defender[T]) Cleanup() {
+	d.Lock()
+	defer d.Unlock()
+
+	for key, client := range d.clients {
+		if client.Expired() {
+			delete(d.clients, key)
+		}
+	}
+}
